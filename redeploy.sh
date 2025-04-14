8c068905add776cd83525288b6dd509781f5b689
Connor <moyerc05@moravian.edu>
2025-04-14 19:47:25 -0400

    fix redeploy file

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,5 @@
 cd /home/ec2-user/Finn-Connor-AidanProject
+sudo systemctl stop discord_bot
 sudo git pull origin main
-mvn clean package -DskipTests
-sudo systemctl restart discord_bot
+sudo mvn clean package -DskipTests
+sudo systemctl start discord_bot
