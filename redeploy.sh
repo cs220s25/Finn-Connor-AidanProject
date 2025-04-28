f4b02186996faf2984d5c5afc1f26109d0b7966a
Connor Moyer <moyerc05@moravian.edu>
2025-04-28 16:26:38 -0400

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,4 @@
 cd /home/ec2-user/Finn-Connor-AidanProject
-sudo systemctl stop discord_bot
 sudo git pull origin main
-sudo mvn clean package -DskipTests
-sudo systemctl start discord_bot
+sudo docker compose down
+sudo docker compose up -d --build
