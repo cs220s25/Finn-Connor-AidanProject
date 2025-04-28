8a6f0af3ab4cc788027a41b2f29659f415d08226
Connor Moyer <moyerc05@moravian.edu>
2025-04-28 16:24:22 -0400

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-sudo yum install -y maven-amazon-corretto21
+# Install Docker
+sudo yum install -y docker
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -a -G docker ec2-user
+
+# Install docker compose plugin
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.35.1/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
+# Install git
 sudo yum install -y git
+
+# Clone the repo
 cd /home/ec2-user
 git clone https://github.com/cs220s25/Finn-Connor-AidanProject.git
 cd Finn-Connor-AidanProject/
-mvn clean package  
-sudo cp discord_bot.service /etc/systemd/system
-sudo systemctl enable discord_bot.service
-sudo systemctl start discord_bot.service
+
+# Build and run the docker containers
+sudo docker compose up -d
